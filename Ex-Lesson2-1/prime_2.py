76182fde772e1939cfd7a7c8582921944d589095
s21023 <s21023@std.it-college.ac.jp>
2023-05-16 10:36:50 +0900

    EX_2-1を追加

---
+++ Ex-Lesson2-1/prime_2.py
@@ -0,0 +1,31 @@
+p = [True] * 81
+p[0] = False
+p[1] = False
+n = 2
+
+def hyou():
+    s = ""
+    for i in range(81):
+        if p[i] == True:
+            s += "{:2d}, ".format(i)
+        else:
+            s += "/, "
+        if i % 9 == 8:
+            s += "¥n"
+    print(s)
+
+def furui():
+    global n
+    for i in range(n+n, 81, n):
+        p[i] = False
+    print(n, "の倍数をふるい落としました")
+    hyou()
+    while n < 81 :
+        n = n + 1
+        if p[n] == True:
+            break
+
+hyou()
+while n < 9:
+    input("Enterキーを押せ")
+    furui()
