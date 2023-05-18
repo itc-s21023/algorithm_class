3563b10a0069d7b2ee2387da7885a2f4ae2790cf
s21023 <s21023@std.it-college.ac.jp>
2023-05-18 10:05:25 +0900

    queue_1.py修正

--- Lesson3/Lesson3-2/queue_1.py
+++ Lesson3/Lesson3-2/queue_1.py
@@ -1,37 +1,44 @@
-MAX = 6  #キューの最大指定
-que = [0] * MAX
+MAX_SIZE = 6
+queue = [0] * MAX_SIZE
 head = 0
 tail = 0
 
-def enqueue(d):
+def get_queue_contents():
+    i = head
+    contents = []
+    while i % MAX_SIZE != tail: # キューの要素を順番に取得
+        contents.append(queue[i])
+        i = (i + 1) % MAX_SIZE
+    return contents
+
+def enqueue(data):
     global tail
-    nt = (tail + 1) % MAX
-    if nt == head:  #キューの空きがないなら
-        print("これ以上入れられません")
-    else:  #空きがあるなら
-        que[tail] = d
-        tail = nt
-        print(d, "人待ちに入る")
-        wait_time = (tail - head) * 15 ##待ち時間を表示する
-        print("待ち時間:", wait_time, "分")
+    next_tail = (tail + 1) % MAX_SIZE # 次の末尾インデックスを計算
+
+    if next_tail == head:
+        print('待ち列がいっぱいです')
+    else:
+        queue[tail] = data
+        tail = next_tail
+        count = len(get_queue_contents()) # 現在の待ち時間を計算
+        print(f'待ち時間: {count * 15}分')
 
 def dequeue():
     global head
     if head == tail:
-        print("取り出すデータがない")
+        print('取り出すデータが存在しません')
         return None
     else:
-        d = que[head]
-        head = (head + 1) % MAX
-        print("待ちから", d, "人出る")
-        wait_time = (tail - head) * 15
-        print("待ち時間:", wait_time, "分")  #待ち時間を表示する
-        return d
+        data = queue[head]
+        queue[head] = 0
+        head = (head + 1) % MAX_SIZE
+        print(f'{data}が列から出ました')
+        return data
 
+for i in range(3):
+    enqueue(i)
 
-enqueue(1)
-enqueue(2)
-enqueue(3)
-dequeue()
-dequeue()
-enqueue(4)
+for i in range(2):
+    dequeue()
+
+enqueue(i)
