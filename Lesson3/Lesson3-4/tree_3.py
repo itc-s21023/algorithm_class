b173055afb522c1f4cdf07ed785d1ebe9ade6e93
s21023 <s21023@std.it-college.ac.jp>
2023-05-23 12:40:47 +0900

    tree_3.py修正

--- Lesson3/Lesson3-4/tree_3.py
+++ Lesson3/Lesson3-4/tree_3.py
@@ -16,14 +16,12 @@ MAX = len(node)
 print("指定の番号のノードを調べます")
 print("何も入力せずにEnterを押すと終了します")
 
-s = input("number=")
-if s != "":
-    n = int(s)
-    if 0 <= n < MAX:
-        left_child = node[n][LEFT]
-        data = node[n][DATA]
-
-        print("node{}のLEFTの値は{}".format(n, left_child))
-        print("node{}のDATAの値は{}".format(n, data))
+while True:
+    lf = input("number=")
+    if lf == "":
+        break
+    n = int(lf)
+    if 0 <= n and n < MAX:
+        print("node{}の値は{}".format(n, node[n][DATA]))
     else:
         print("0から{}の範囲で入力してください".format(MAX-1))
